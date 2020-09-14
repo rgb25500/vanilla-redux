c2db5d0ab9c37acbe353d5dfe8491d88ea38cf83
nodemon <ooly9138@gmail.com>
2020-09-14 12:53:12 +0900

    1.0 Vanilla Counter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // vanilla로 시작할 것이므로 일단 다 지우고 시작!
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+number.innerText = count;
+
+const updateText = () => {
+  number.innerText = count;
+};
+
+const handleAdd = () => {
+  count = count + 1;
+  updateText();
+};
+const handleMinus = () => {
+  count = count - 1;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
