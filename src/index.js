264a9089eda475fae6066533216c44e390eb981e
nodemon <ooly9138@gmail.com>
2020-09-14 14:12:28 +0900

    1.1 Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 // vanilla로 시작할 것이므로 일단 다 지우고 시작!
 
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+// data를 바꿔주는 function
+// reducer와 modifier가 return 하는 것이 application의 data가 됨.
+const countModifier = (state = 0) => {
+  return state;
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+// application에서 유일하게 바뀌는 코드
+// let count = 0;
