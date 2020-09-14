7a500576e094f54f97903ccd829b4899b914bceb
nodemon <ooly9138@gmail.com>
2020-09-14 18:05:21 +0900

    #1.4 Recap Refactor

--- src/index.js
+++ src/index.js
@@ -8,17 +8,21 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // data를 바꿔주는 function
 // reducer와 modifier가 return 하는 것이 application의 data가 됨.
 // argument의 count는 디폴트 0에서 시작, dispatch 할 때마다 값이 변함. 현재의 state로.
 const countModifier = (count = 0, action) => {
   // console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -33,11 +37,11 @@ countStore.subscribe(onChange);
 
 add.addEventListener("click", () => {
   // countModifier에 data를 보내는 방법
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 });
 
 minus.addEventListener("click", () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 });
 
 // console.log(countStore.getState());
