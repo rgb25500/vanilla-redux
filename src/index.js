8c7326a4af2c6b3da4b3f1f5b0ed63f09113f205
nodemon <ooly9138@gmail.com>
2020-09-16 21:32:45 +0900

    #2.2 Delete ToDo

--- src/index.js
+++ src/index.js
@@ -7,11 +7,23 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// 오직 action만 return 함
+const addToDo = (text) => {
+  return { type: ADD_TODO, text };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [...state, { text: action.text, id: Date.now() }]; // 과거의 state와 새로운 TODO를 갖고 있음.
+      return [{ text: action.text, id: Date.now() }, ...state]; // 과거의 state와 새로운 TODO를 갖고 있음.
     case DELETE_TODO:
       return [];
     default:
@@ -25,12 +37,38 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
+const dispatchAddToDo = (text) => {
+  //   createToDo(toDo);   dispatch 시킬 것임.
+  store.dispatch(addToDo(text));
+};
+
+const dispatchDeleteToDo = (e) => {
+  // console.log("delete");
+  const id = e.target.parentNode.id; // 클릭되는 버튼 target
+  store.dispatch(deleteToDo(id));
+};
+
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = ""; // toDos가 계속 repaint되기 때문에
+  toDos.forEach((toDo) => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+store.subscribe(paintToDos);
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  //   createToDo(toDo);   dispatch 시킬 것임.
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
