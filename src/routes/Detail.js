64ce62a900ffce1d5cc968d6818d27e84c47b248
nodemon <ooly9138@gmail.com>
2020-09-25 23:35:35 +0900

    #4.1 CreateAction

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,24 @@
-export default () => "Detail";
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({ toDo }) {
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Created at: {toDo?.id}</h5>
+    </>
+  );
+}
+
+function mapStateToProps(state, ownProps) {
+  const {
+    match: {
+      params: { id },
+    },
+  } = ownProps;
+  console.log(id);
+  return { toDos: state.find((toDo) => toDo.id === parseInt(id)) };
+}
+// Array.find() : testing function에 만족하는 첫번째 요소를 반환
+
+export default connect(mapStateToProps)(Detail);
--- src/store.js
+++ src/store.js
@@ -1,28 +1,41 @@
 import { createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// 4.2 : createAction
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//   };
+// };
 
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id: parseInt(id),
-  };
-};
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id: parseInt(id),
+//   };
+// };
+
+// createAction shortcut
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
+
+console.log(addToDo, deleteToDo); // function
+console.log(addToDo.type, deleteToDo.type); // text
+console.log(addToDo(), deleteToDo()); // object
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    // case ADD:
+    case addToDo.type:
+      console.log(action);
+      return [{ text: action.payload, id: Date.now() }, ...state];
+    // case DELETE:
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
