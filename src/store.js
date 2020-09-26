c8114c86698167801ddea03c62e4cd9a51e24987
nodemon <ooly9138@gmail.com>
2020-09-26 12:22:30 +0900

    #4.2 createReducer

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // 4.2 : createAction
 // const ADD = "ADD";
@@ -27,19 +27,33 @@ console.log(addToDo, deleteToDo); // function
 console.log(addToDo.type, deleteToDo.type); // text
 console.log(addToDo(), deleteToDo()); // object
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    // case ADD:
-    case addToDo.type:
-      console.log(action);
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    // case DELETE:
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// 4.3 : createReducer
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     // case ADD:
+//     case addToDo.type:
+//       console.log(action);
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     // case DELETE:
+//     case deleteToDo.type:
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+// createReducer shortcut
+const reducer = createReducer([], {
+  // 여기서 state는 empty array
+  [addToDo]: (state, action) => {
+    // push는 state를 mutate 해주고, 아무것도 return 하지 않음.
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteToDo]: (state, action) => {
+    // 새로운 state를 return 함.
+    return state.filter((toDo) => toDo.id !== action.payload);
+  },
+});
 
 const store = createStore(reducer);
 
